3e22e3e909d01f980fc1bb089d0d146aef7c6919
pmcdivyadanane-netizen <pmcdivyadanane@gmail.com>
2026-07-13 19:34:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -71,14 +71,13 @@ def home(sheet_name=None):
 
     reports = get_report_sheets()
 
-if not reports:
-    return render_template(
-        "index.html",
-        reports=[],
-        selected_sheet=None,
-        sheet_data=[],
-        report_image=""
-    )
+    if not reports:
+        return render_template(
+            "index.html",
+            reports=[],
+            selected_sheet=None,
+            sheet_data=[]
+        )
 
     if sheet_name is None:
         sheet_name = reports[0]
